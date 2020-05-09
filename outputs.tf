ab6485a7dc9e5ded8bbc5945e21995df7223aa11
Anatol.Kozel <Anatol.Kozel@nike.com>
2020-05-09 14:05:11 +0300

    resolved outdated issues. Removed unused roles/policies

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,4 @@
 output "user_data" {
   description = "The user_data with the cloudwatch_agent configuration in base64 and gzipped"
-  value       = "${data.template_cloudinit_config.cloud_init_merged.rendered}"
-}
-
-output "role_name" {
-  description = "The name of the created IAM role that can be assumed by the instance"
-  value       = "${aws_iam_role.ec2_cloudwatch.name}"
-}
-
-output "iam_policy_document" {
-  description = "The IAM policy document that can be attached to a role policy"
-  value       = "${data.aws_iam_policy_document.wildcard_cloudwatch_agent.json}"
+  value       = data.template_cloudinit_config.cloud_init_merged.rendered
 }
